509c0c41da1edfbf62483f2dc896d5d0dc7858f6
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-01-14 16:47:27 -0500

    seventh commit

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -9,7 +9,16 @@ import com.apcsa.model.User;
 public class Administrator extends User {
 
     public Administrator(User user, ResultSet rs) throws SQL Exception {
+      super(user);
+
+	    this.administratorId = rs.getInt("administrator_id");
+	    this.firstName = rs.getString("first_name");
+	    this.lastName = rs.getString("last_name");
+	    this.jobTitle = rs.getString("job_title");
       }
+    public String getFirstName() {
+      return firstName;
+    }
     private int administratorId;
     private String firstName;
     private String lastName;
