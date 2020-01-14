79f851e54aa3a611d384170aa157c5c27670d2b0
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-01-14 16:39:03 -0500

    second commit

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -1,9 +1,13 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+
 import com.apcsa.model.User;
 
 public class Administrator extends User {
 
+    public Administrator(User user, ResultSet rs) {
+      }
     private int administratorId;
     private String firstName;
     private String lastName;
