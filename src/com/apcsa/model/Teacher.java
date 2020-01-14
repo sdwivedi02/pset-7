e90ef905f98a7cb0eed88fc3f3cd666c7c7eb066
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-01-14 16:42:37 -0500

    fourth commit

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,9 +1,14 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+
+
 import com.apcsa.model.User;
 
 public class Teacher extends User {
 
+    public Teacher(User user, ResultSet rs) {
+      }
     private int teacherId;
     private int departmentId;
     private String firstName;
