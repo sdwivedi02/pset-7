fab0258e5b001e206622dd54bb67eb4df90ae1bd
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-01-14 17:02:55 -0500

    thirteenth commit

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -2,9 +2,23 @@ package com.apcsa.model;
 
 import java.sql.ResultSet;
 
+import java.sql.SQLException;
+
 import com.apcsa.model.User;
 
 public class Student extends User {
+
+  public Student(User user, ResultSet rs) throws SQLException {
+	    super(user);
+
+	    this.studentId = rs.getInt("student_id");
+	    this.classRank = rs.getInt("class_rank");
+	    this.gradeLevel = rs.getInt("grade_level");
+	    this.graduationYear = rs.getInt("graduation");
+	    this.gpa = rs.getDouble("gpa");
+	    this.firstName = rs.getString("first_name");
+	    this.lastName = rs.getString("last_name");
+	}
 
     private int studentId;
     private int classRank;
@@ -13,7 +27,5 @@ public class Student extends User {
     private double gpa;
     private String firstName;
     private String lastName;
-    public Student(User user, ResultSet rs) {
-	}
 
 }
