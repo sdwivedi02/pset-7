3eff447b711e37bd9077a7afd50282d1d7f2b343
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-01-14 16:55:02 -0500

    ninth commit

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -4,10 +4,17 @@ import java.sql.ResultSet;
 
 
 import com.apcsa.model.User;
+import java.sql.SQLException;
 
 public class Teacher extends User {
 
-    public Teacher(User user, ResultSet rs) {
+    public Teacher(User user, ResultSet rs) throws SQLException {
+      super(user);
+
+      this.teacherId = rs.getInt("teacher_id");
+      this.departmentId = rs.getInt("department_id");
+      this.firstName = rs.getString("first_name");
+      this.lastName = rs.getString("last_name");
       }
     private int teacherId;
     private int departmentId;
